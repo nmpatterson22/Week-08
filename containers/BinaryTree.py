84eb78518264c9bd7ea424258d233a57c968a70e
nmpatterson22 <22mpatterson@gmail.com>
2021-04-25 22:21:48 -0700

    Done

--- containers/BinaryTree.py
+++ containers/BinaryTree.py
@@ -194,7 +194,7 @@ class BinaryTree():
         HINT:
         See how the __len__ method calls its helper staticmethod.
         '''
-        return BinaryTree._height(self.root)
+        return BinaryTree._height(self.root) - 1
 
     @staticmethod
     def _height(node):
@@ -210,9 +210,8 @@ class BinaryTree():
         right _heights calculated above
         '''
         if node:
-            return -1
-            left = BinaryTree._height(node.left) + 1
-            right = BinaryTree._height(node.right) + 1
-            return max(left, right)
+            left_height = BinaryTree._height(node.left) + 1
+            right_height = BinaryTree._height(node.right) + 1
+            return max(left_height, right_height)
         else:
             return 0
